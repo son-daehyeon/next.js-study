ef73421a177fd28d821d60b02720a3f5c0e68cbf
daun-up <awhekdns@kookmin.ac.kr>
2024-05-12 23:08:07 +0900

    Styles. css 수정

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -27,6 +27,8 @@
 // }
 
 import Link from 'next/link';
+import Movie from '../components/movie';
+import styles from '../../styles/home.module.css';
 
 export const metadata = {
   title: 'Home',
@@ -44,11 +46,9 @@ async function getMovies() {
 export default async function HomePage() {
   const movies = await getMovies();
   return (
-    <div>
+    <div className={styles.container}>
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+        <Movie key={movie.id} id={movie.id} poster_path={movie.poster_path} title={movie.title} />
       ))}
     </div>
   );
--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,20 +1,21 @@
-"use client"
+'use client';
 
-import Link from "next/link";
-import {usePathname} from "next/navigation";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import styles from '../../styles/navigation.module.css';
 
 export default function Navigation() {
-    const path = usePathname()
-    return (
-    <nav>
-        <ul>
-            <li>
-                <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
-            </li>
-            <li>
-                <Link href="/about-us">About Us</Link> {path === "/about-us" ? "🔥" : ""}
-            </li>
-        </ul>
+  const path = usePathname();
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <Link href="/">Home</Link> {path === '/' ? '🔥' : ''}
+        </li>
+        <li>
+          <Link href="/about-us">About Us</Link> {path === '/about-us' ? '🔥' : ''}
+        </li>
+      </ul>
     </nav>
-    );
+  );
 }
